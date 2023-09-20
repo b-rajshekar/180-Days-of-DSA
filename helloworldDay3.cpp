760e2f720bbd59e04af3372242666c58d9dceae1
Bujjagowns Rajashekhar <91706770+b-rajshekar@users.noreply.github.com>
2023-09-20 12:23:34 +0530

    Add files via upload

---
+++ helloworldDay3.cpp
@@ -0,0 +1,30 @@
+// Day 3/180
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout<<"Hola Mundo\n";
+    cout<<4+3;
+    
+    // char c = 'a';
+    // cout<<c;
+
+    // float f = 1.27;
+    // cout<<f;
+
+    // double d = 3.121;
+    // cout<<d;
+
+    // bool b = 0;
+    // cout<<b;
+};
+
+/*
+Today is my day three of learning DSA from youtube and what I have learned today is:
+--> How aplhabet stores in memory.
+--> ASCII Table.
+--> learned basic syntax of C++ and wrote the above program.
+--> Learned about different data types and variables in C++.
+*/
