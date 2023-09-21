77cd4f104482fe2ca308f314181affaa06e42354
Bujjagowns Rajashekhar <91706770+b-rajshekar@users.noreply.github.com>
2023-09-21 12:03:34 +0530

    Add files via upload

---
+++ Day4_+-.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+    cout<<"Enter a number: ";
+    cin>>num;
+
+    if(num>0) {
+        cout<<"Positivo";
+    }
+    
+     else if(num==0) {
+        cout<<"el cero";
+
+     } else {
+        cout<<"negativo";
+     }
+}
