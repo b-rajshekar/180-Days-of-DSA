b02aba31950bbca323f2d4e7763bf4f66630da87
Bujjagowns Rajashekhar <91706770+b-rajshekar@users.noreply.github.com>
2023-09-25 12:37:20 +0530

    Add files via upload

---
+++ Day8_sumofNnatural.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n,i, sum=0;
+    cout<<"Enter the number: ";
+    cin>>n;
+
+// Guddu
+    // for(i=1; i<=n; i=i+1)
+    // {
+    //     sum = sum+i;
+    // }
+
+// Babblu
+cout<<(n*(n+1))/2;
+
+}
